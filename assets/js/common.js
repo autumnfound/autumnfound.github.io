e37a82af7b62ae2f2f0ca12ca948228a4823b1b2
autumnfound <mtlowe@live.ca>
2018-09-08 13:07:24 -0400

    Switched to using Jekyll for project rather than static html.

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,47 +1,5 @@
 var $ = jQuery;
 
 $( document ).ready(function() {
-    includes();
+    // something will go here. Probably carousels and stuff
 });
-
-// dynamically include files based on data attribute
-function includes() {
-    var includes = $('[data-include]');
-    $.each(includes, function(){
-      var $t = $(this);
-      var file = '/assets/templates/' + $(this).data('include') + '.html';
-
-      // retrieve this methods callback (when present)
-      var callback = $t.data('callback');
-
-      $t.load(file, function() {
-          if (callback != undefined) {
-              window[callback]();
-          }
-      });
-
-      // clear the attribute
-      $(this).removeAttr('data-include');
-      $(this).removeAttr('data-callback');
-    });
-}
-
-function setTitle(col) {
-    // update the page titles to use the page name
-    var pageName = window.location.href.match(/.*\/([^\.]*?)\..*$/)[1];
-    // index home can have no URL, so if none is found assume Index page
-    if (pageName == null) {
-        pageName = "Index";
-    } else {
-        // replace dashes and do casing
-        pageName = pageName.replace('-', ' ');
-        pageName = pageName.replace(/(^\w|\s[a-zA-Z])/g, function(match, str) {
-            return str.toUpperCase();
-        });
-    }
-
-    // replace the placeholder with the new value
-    var titleText = $('title').text().replace(/\{PAGE_NAME\}/, pageName);
-    $('title').text(titleText);
-    $("meta[name='title']").attr('value',titleText);
-}
